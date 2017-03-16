35299566e8929c7151e73805774f18eeb7ef0fc1
Sebastian Manger <s.manger@gmx.ch>
2017-03-16 15:37:43 +0100

    1137 Write emails to file during development

--- apps/qcat/settings.py
+++ apps/qcat/settings.py
@@ -1,10 +1,13 @@
+from os.path import join
+
 from .config.common import BaseSettings
 from .config.mixins import CompressMixin, DevMixin, SentryMixin, ProdMixin, \
     SecurityMixin, LogMixin, TestMixin
 
 
 class DevDefaultSite(DevMixin, BaseSettings):
-    pass
+    EMAIL_BACKEND = 'eml_email_backend.EmailBackend'
+    EMAIL_FILE_PATH = join(BaseSettings.BASE_DIR, 'tmp')
 
 
 class TestDefaultSite(TestMixin, DevDefaultSite):
