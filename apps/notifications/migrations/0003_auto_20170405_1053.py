971aa6962778b2303b637fc4b5a38d1b51d817e7
Sebastian Manger <s.manger@gmx.ch>
2017-04-05 10:56:28 +0200

    1137 Clean up migrations

--- apps/notifications/migrations/0003_auto_20170405_1053.py
+++ apps/notifications/migrations/0003_auto_20170405_1053.py
@@ -1,20 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.core.management import call_command
 from django.db import migrations, models
 import notifications.validators
 from django.conf import settings
-
-
-def load_defaults(apps, schema_editor):
-    # model methods can't be accessed during migrations, so the management
-    # command is used
-    call_command('set_mail_defaults')
-
-
-def purge_defaults(apps, schema_editor):
-    pass
 
 
 class Migration(migrations.Migration):
@@ -30,10 +19,20 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
                 ('subscription', models.CharField(max_length=10, default='all', choices=[('none', 'No emails at all'), ('todo', 'Only emails that I need to work on'), ('all', 'All emails')])),
-                ('wanted_actions', models.CharField(max_length=255, blank=True, validators=[notifications.validators.clean_wanted_actions])),
+                ('wanted_actions', models.CharField(verbose_name='Subscribed for following changes in the review status', max_length=255, blank=True, validators=[notifications.validators.clean_wanted_actions])),
                 ('language', models.CharField(max_length=2, default='en', choices=[('en', 'English'), ('fr', 'French'), ('es', 'Spanish'), ('ru', 'Russian'), ('km', 'Khmer'), ('lo', 'Lao'), ('ar', 'Arabic'), ('bs', 'Bosnian'), ('pt', 'Portuguese')])),
+                ('has_changed_language', models.BooleanField(default=False)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
         ),
-        migrations.RunPython(load_defaults, purge_defaults)
+        migrations.AddField(
+            model_name='log',
+            name='was_sent',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='readlog',
+            name='is_deleted',
+            field=models.BooleanField(default=False),
+        ),
     ]
